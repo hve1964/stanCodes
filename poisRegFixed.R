e8f3a2e06be8957bde7321fd90c6306eab564d00
Henk van Elst <29248648+hve1964@users.noreply.github.com>
2020-08-05 11:59:03 +0200

    Fixed effects Poisson regression.

--- poisRegFixed.R
+++ poisRegFixed.R
@@ -1,6 +1,6 @@
 ################################################################################
 # Lecture notes: test of poisReg Stan code (fixed priors)
-# Mo, 03.08.2020
+# Mi, 05.08.2020
 # Data: plane touch down numbers
 ################################################################################
 
@@ -16,6 +16,7 @@ library(bayesplot)
 options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = TRUE)
 Sys.setenv(LOCAL_CPPFLAGS = "-march=corei7 -mtune=corei7")
+stan_version()
 
 #-------------------------------------------------------------------------------
 # Load data
@@ -51,8 +52,6 @@ head( X , n = 10 )
 #-------------------------------------------------------------------------------
 # Specify data list for Stan simulation
 #-------------------------------------------------------------------------------
-y <- poissonData$y
-
 dataList <- list(
   N = nrow(X) ,
   M = ncol(X) ,
@@ -64,7 +63,7 @@ rm( poissonData , X , Z )
 #-------------------------------------------------------------------------------
 # Fitting a Stan model: Poisson likelihood w/ log link and fixed priors
 #-------------------------------------------------------------------------------
-mod1.stan <- stan(
+modelStan <- stan(
   file = "poisRegFixed.stan" ,
   data = dataList ,
   chains = 4 ,
@@ -78,62 +77,64 @@ mod1.stan <- stan(
   cores = 3
 )
 
-class(mod1.stan)
-dim(mod1.stan)
+class(modelStan)
+dim(modelStan)
 
 #-------------------------------------------------------------------------------
 # Summary and MCMC diagnostics
 #-------------------------------------------------------------------------------
 print(
-  x = mod1.stan ,
+  x = modelStan ,
   pars = c("beta", "lp__") ,
   probs = c(0.015, 0.25, 0.50, 0.75, 0.985)
 )
 
-check_hmc_diagnostics(mod1.stan)
+check_hmc_diagnostics(modelStan)
 stan_trace(
-  object = mod1.stan ,
+  object = modelStan ,
   pars = c("beta", "lp__") ,
   inc_warmup = TRUE
 )  # "rstan"
 stan_plot(
-  object = mod1.stan ,
+  object = modelStan ,
   pars = c("beta") ,
   ci_level = 0.89 ,
   outer_level = 0.97
 )  # "rstan"
 stan_dens(
-  object = mod1.stan ,
+  object = modelStan ,
   pars = c("beta", "lp__")
 )  # "rstan"
 
 plot_title <- ggtitle( "Posterior marginal distributions" ,
                        "with medians and 89% intervals")
 mcmc_areas(
-  x = mod1.stan ,
+  x = modelStan ,
   regex_pars = c("beta") ,
   prob = 0.89
 ) + plot_title
+# bayesplot"
 
-np <- nuts_params(mod1.stan)
+np <- nuts_params(modelStan)
 mcmc_nuts_energy(np) + ggtitle("NUTS Energy Diagnostic")
+# bayesplot"
 
 pairs(
-  x = mod1.stan ,
+  x = modelStan ,
   pars = c("beta")
 )  # "rstan"
 
 mcmc_scatter(
-  x = as.matrix(mod1.stan) ,
+  x = as.matrix(modelStan) ,
   pars = c("beta[1]", "beta[2]")
 )  # bayesplot"
 mcmc_scatter(
-  x = as.matrix(mod1.stan) ,
+  x = as.matrix(modelStan) ,
   pars = c("beta[3]", "beta[4]")
 )  # bayesplot"
 
 mcmc_hex(
-  x = as.matrix(mod1.stan) ,
+  x = as.matrix(modelStan) ,
   pars = c("beta[3]", "beta[4]")
 )  # bayesplot"
 
@@ -141,7 +142,7 @@ mcmc_hex(
 # Posterior predictive checks
 #-------------------------------------------------------------------------------
 draws <- as.matrix(
-  mod1.stan ,
+  modelStan ,
   pars = "yrep"
 )
 class(draws)
@@ -164,13 +165,13 @@ ppc_stat(
   yrep = draws ,
   stat = "max" ,
   binwidth = 1
-)
+)  # bayesplot"
 ppc_stat(
   y = dataList$y ,
   yrep = draws ,
   stat = "mean" ,
   binwidth = 0.1
-)
+)  # bayesplot"
 ppc_intervals(
   y = dataList$y ,
   yrep = draws , 

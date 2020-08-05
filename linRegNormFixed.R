d6f68734a83bcf82d041a9603771115f8fac98c3
Henk van Elst <29248648+hve1964@users.noreply.github.com>
2020-08-05 10:42:25 +0200

    Fixed effects multiple linear regression.

--- linRegNormFixed.R
+++ linRegNormFixed.R
@@ -16,6 +16,7 @@ library(bayesplot)
 options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = TRUE)
 Sys.setenv(LOCAL_CPPFLAGS = "-march=corei7 -mtune=corei7")
+stan_version()
 
 #-------------------------------------------------------------------------------
 # Load data

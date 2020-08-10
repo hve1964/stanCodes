05c4792152983f87b2226d3a1a7e10d0e864eab8
Henk van Elst <29248648+hve1964@users.noreply.github.com>
2020-08-10 12:48:02 +0200

    Varying effects Poisson regression.

--- poisRegVarying.R
+++ poisRegVarying.R
@@ -96,19 +96,19 @@ dim(modelStan)
 #-------------------------------------------------------------------------------
 print(
   x = modelStan ,
-  pars = c("beta", "gamma", "R", "lp__") ,
+  pars = c("beta", "gamma_beta", "R", "lp__") ,
   probs = c(0.015, 0.25, 0.50, 0.75, 0.985)
 )
 
 check_hmc_diagnostics(modelStan)
 stan_trace(
   object = modelStan ,
-  pars = c("beta", "gamma", "R", "lp__") ,
+  pars = c("beta", "gamma_beta", "R", "lp__") ,
   inc_warmup = TRUE
 )  # "rstan"
 stan_plot(
   object = modelStan ,
-  pars = c("beta", "gamma") ,
+  pars = c("beta", "gamma_beta") ,
   ci_level = 0.89 ,
   outer_level = 0.97
 )  # "rstan"
@@ -120,14 +120,14 @@ stan_plot(
 )  # "rstan"
 stan_dens(
   object = modelStan ,
-  pars = c("beta", "gamma", "R", "lp__")
+  pars = c("beta", "gamma_beta", "R", "lp__")
 )  # "rstan"
 
 plot_title <- ggtitle( "Posterior marginal distributions" ,
                        "with medians and 89% intervals")
 mcmc_areas(
   x = modelStan ,
-  regex_pars = c("beta", "gamma") ,
+  regex_pars = c("beta", "gamma_beta") ,
   prob = 0.89
 ) + plot_title
 # bayesplot"

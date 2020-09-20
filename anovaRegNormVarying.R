ff17cf5be22b9ab11c577644dac3d7480491ed26
Henk van Elst <29248648+hve1964@users.noreply.github.com>
2020-09-20 14:07:53 +0200

    Varying effects heteroscedastic ANOVA-like regression.

--- anovaRegNormVarying.R
+++ anovaRegNormVarying.R
@@ -16,7 +16,6 @@ library(bayesplot)
 library(loo)
 options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = TRUE)
-Sys.setenv(LOCAL_CPPFLAGS = "-march=corei7 -mtune=corei7")
 stan_version()
 
 #-------------------------------------------------------------------------------

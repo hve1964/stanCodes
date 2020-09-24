078e1642188d0f7fb73e3f22c579ba3015d9d9b2
Henk van Elst <29248648+hve1964@users.noreply.github.com>
2020-09-24 19:09:21 +0200

    Fixed effects logistic regression.

--- logistRegBernFixed.R
+++ logistRegBernFixed.R
@@ -15,7 +15,6 @@ rstan_options("required" = FALSE)
 library(bayesplot)
 options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = TRUE)
-Sys.setenv(LOCAL_CPPFLAGS = "-march=corei7 -mtune=corei7")
 stan_version()
 
 #-------------------------------------------------------------------------------
